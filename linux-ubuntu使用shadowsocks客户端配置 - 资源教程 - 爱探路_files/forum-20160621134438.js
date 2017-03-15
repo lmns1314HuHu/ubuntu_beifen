fe99972e73c8359c663d728806053e112f9cb152
yunhao <851549562@qq.com>
2017-03-15 11:11:27 +0800

    shadowsocks

---
+++ linux-ubuntu使用shadowsocks客户端配置 - 资源教程 - 爱探路_files/forum-20160621134438.js
@@ -0,0 +1,8 @@
+var opz="",opfz="",qz=11,ggwid="forum-20160621134438",atyh=0,amsdir="https://adman.henghost.com/",datadir="data/",amsurl='<script charset="utf-8" language="JavaScript" type="text/javascript" src="'+amsdir;
+/*begin book-20160621134427*/
+var wllx=1,clqz=5,ifout=1,clout="",atyh=0;
+if(ifout){clout=''+amsurl+datadir+'thread-20160621134416.js"></script>';if(clout){}else{}}
+if(clout){if(wllx==2){opfz=opfz+clout;}else{if(!opz){opz=clout;qz=clqz;}else{if(clqz<qz){opz=clout;qz=clqz;}}}}
+/*end book-20160621134427*/
+/*end ggcl*//*begin bjgg*/
+document.write(opz+opfz);
---
+++ linux-ubuntu使用shadowsocks客户端配置 - 资源教程 - 爱探路_files/getJScode.asp
@@ -0,0 +1,6 @@
+
+            document.write('<a href="http://www.west.cn?ReferenceID=817826" target="_blank">');
+            document.write('<img src="http://www.west.cn/vcp/vcp_img/free6/F/250x250_F.jpg"  width="250"  height="250"  border=0>');
+            document.write('</a>');
+
+            
